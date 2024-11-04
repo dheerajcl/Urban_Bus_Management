dd98484390435fedc810428ae8ca8788f2f03b09
dheerajcl <cldheeraj541@gmail.com>
2024-11-04 19:57:42 +0530

    resolved toast bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/react"
 import { AuthProvider } from "@/lib/auth";
+import { Toaster } from "@/components/ui/toaster"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -44,6 +45,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </AuthProvider>
+        <Toaster />
         <Analytics />
       </body>
     </html>
