611de30dc07c964384e490af48992f2b969db9fb
Dhanush M <dhanush13589@gmail.com>
2024-11-05 11:40:44 +0530

    Lint error resolving..

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Search, Bus, Clock, Users, CreditCard, MapPin, Calendar as CalendarIcon } from 'lucide-react'
