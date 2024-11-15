649b4c227c8ed13cce5e1fe555deb8199aa60d2e
dheerajcl <cldheeraj541@gmail.com>
2024-11-15 11:43:06 +0530

    Lint issues

--- components/Charts.tsx
+++ components/Charts.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 'use client'
 
 import { Line, LineChart, ResponsiveContainer, XAxis, YAxis, Tooltip } from "recharts"
--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 "use client"
 
 import * as React from "react"
