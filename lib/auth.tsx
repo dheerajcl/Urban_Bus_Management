17571aded372391f0baf2197d5380c9663f37b6b
dheerajcl <cldheeraj541@gmail.com>
2024-11-16 13:28:00 +0530

    Issue #19 resolved

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react'
-import { useRouter, usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { Loader } from 'lucide-react'
 
 interface User {
